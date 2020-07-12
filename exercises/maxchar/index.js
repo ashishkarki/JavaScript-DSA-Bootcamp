38e235ca66ef5b40f67d71cfeff70e1a7954bc3a
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-07-12 18:37:55 +0545

    Implemented  solution for 'maxChar'

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,49 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {}
+    let max = 0
+    let maxCharacter = ''
 
-module.exports = maxChar;
+    for (let char of str) {
+        // if (chars[ char ]) {
+        //     chars[ char ]++
+        // } else {
+        //     chars[ char ] = 1
+        // }
+        charMap[ char ] = charMap[ char ] + 1 || 1
+    }
+
+    // use for..in to iterate in object: https://alligator.io/js/for-of-for-in-loops/
+    // for (let char in charMap) {
+    //     if (charMap[ char ] > max) {
+    //         max = charMap[ char ]
+    //         maxCharacter = char
+    //     }
+    // }
+    // return maxCharacter
+
+    const sorted = Object.entries(charMap).sort((a, b) => b[ 1 ] - a[ 1 ])
+    return sorted[ 0 ][ 0 ]
+}
+
+module.exports = maxChar
+
+/*
+// SOLUTION 1
+    // * MY SOLUTION
+    const map = new Map()
+    for (let char of str.split('')) {
+        if (null !== map.get(char)) {
+            map.set(char, map.get(char) + 1)
+        } else {
+            map.set(char, 1)
+        }
+        debugger
+    }
+
+    const sortedMap = [ ...map.entries() ].sort((a, b) => b[ 1 ] - a[ 1 ])
+
+    return sortedMap[ 0 ][ 0 ]
+*/
