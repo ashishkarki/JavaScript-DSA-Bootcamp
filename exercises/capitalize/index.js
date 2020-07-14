d50230fb18d3e127380993e0adc7cbbf7f218218
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-07-14 09:41:20 +0545

    Finished 'capitalize'

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,32 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    // MY SOLUTION 1
+    // ! BETTER SOLUTION THAN SOLUTION 2 BELOW
+    // let words = []
 
-module.exports = capitalize;
+    // for (const inputWord of str.split(' ')) {
+    //     words.push(
+    //         inputWord[ 0 ].toUpperCase() +
+    //         inputWord.slice(1)
+    //     )
+    // }
+
+    // return words.join(' ')
+
+    // SOLUTION 2
+    let result = str[ 0 ].toUpperCase()
+    for (let i = 1; i < str.length; i++) {
+        // if the character to the left of current character is a space, then this is a new word, so capitalize te first character of that word
+        if (str[ i - 1 ] === ' ') {
+            result += str[ i ].toUpperCase()
+        } else {
+            result += str[ i ]
+        }
+    }
+
+    return result
+}
+
+module.exports = capitalize
