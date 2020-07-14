0e5a11027d130aa93ce81d8ff9d6f86e0f06a7df
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-07-14 09:17:59 +0545

    Finished 'anagrams'

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,58 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    // SOLUTION 1
+    // ^ means negated, [] means a set or group
+    // this matches all characters which are not in the \w (words => alphabets and underscores) set
+    // meaning this helps find all non-word characters like punctuation
+    /*const nonWordRegex = /[^\w]/g
 
-module.exports = anagrams;
+    // remove special characters and change to lowercase
+    stringA = nonWordsCleaner(stringA)
+    stringB = nonWordsCleaner(stringB)
+
+    // create character map of both and compare for anagram
+    const charMapA = charMapBuilder(stringA)
+    const charMapB = charMapBuilder(stringB)
+
+    // if they don't have same length of words, they aren't anagrams
+    if (Object.keys(charMapA).length !== Object.keys((charMapB)).length) {
+        return false
+    }
+
+    for (let char in charMapA) {
+        if (charMapA[ char ] !== charMapB[ char ]) {
+            return false
+        }
+    }
+
+    return true
+    */
+
+    // SOLUTION 2
+    return stringCleaner(stringA) === stringCleaner(stringB)
+}
+
+function stringCleaner(inputStr) {
+    return inputStr.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
+
+// function nonWordsCleaner(inputStr) {
+//     const nonWordRegex = /[^\w]/g
+
+//     // remove special characters and change to lowercase
+//     return inputStr.replace(nonWordRegex, '').toLowerCase()
+// }
+
+// function charMapBuilder(wordsStr) {
+//     const charMap = {}
+
+//     for (const char of wordsStr) {
+//         charMap[ char ] = (charMap[ char ] + 1) || 1
+//     }
+
+//     return charMap
+// }
+
+module.exports = anagrams
