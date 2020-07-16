4bcb537bdc41fc994934b7544b224dc91a2117f7
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-07-16 12:56:11 +0545

    Finished 'pyramid' with iterative and recursive solutions

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,48 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+    // SOLUTION 1
+    // const colConstraint = 2 * n - 1
+    // const midPoint = Math.floor(colConstraint / 2)
 
-module.exports = pyramid;
+    // for (let row = 0; row < n; row++) {
+    //     let level = '' // stair for each level of pyramid
+    //     for (let column = 0; column < colConstraint; column++) {
+    //         if (column >= midPoint - row &&
+    //             column <= midPoint + row) {
+    //             level += '#'
+    //         } else {
+    //             level += ' '
+    //         }
+    //     }
+
+    //     console.log(level)
+    // }
+
+    // SOLUTION 2: RECURSIVE
+    if (row === n) {
+        return // done printing all rows
+    }
+
+    if (level.length == (2 * n - 1)) {
+        console.log(level)
+
+        // move to new row
+        return pyramid(n, row + 1, '')
+    }
+
+    // when to print '#'
+    const levlLen = level.length
+    const midPoint = Math.floor((2 * n - 1) / 2)
+    if (levlLen >= midPoint - row && levlLen <= midPoint + row) {
+        level += '#'
+    } else {
+        level += ' '
+    }
+
+    // condition for same row, but new column (or level.length)
+    pyramid(n, row, level)
+}
+
+module.exports = pyramid
