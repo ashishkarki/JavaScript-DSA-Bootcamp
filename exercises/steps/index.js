1be1cbb4bbb4c9a0fd0c2f13c45dfc3e7317633d
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-07-16 11:58:02 +0545

    Finished 'steps' with iterative and recursive solutions

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,45 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+    // REF SOLUTION 1
+    // realize that we are creating a square of stairs
+    // for (let row = 0; row < n; row++) {
+    //     let stair = ''
+    //     for (let column = 0; column < n; column++) {
+    //         if (column <= row) { // * very important logic
+    //             stair += '#'
+    //         } else {
+    //             stair += ' '
+    //         }
+    //     }
 
-module.exports = steps;
+    //     console.log(stair)
+    // }
+
+    // REF SOLUTION 2 (WITH RECURSION)
+
+    // the base case to return/end recursion
+    if (row === n) {
+        return // all rows are now printed
+    }
+
+    // do some core logic
+    if (n === stair.length) {
+        // current column is assembled, print it out
+        console.log(stair)
+
+        // move to new row i.e. recurse with some updated condition
+        return steps(n, row + 1, '') // use an empty stair
+    }
+
+    if (stair.length <= row) { // col.len <= row length
+        stair += '#'
+    } else {
+        stair += ' '
+    }
+
+    steps(n, row, stair) // current row with its building stair
+}
+
+module.exports = steps
