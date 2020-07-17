d9b1f4b88fadd4f3b2d86e76784f1c512dc0fbbc
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-07-17 16:54:00 +0545

    Finished 'matrix' implementation

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,54 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const results = []
 
-module.exports = matrix;
+    for (let i = 0; i < n; i++) {
+        results.push([])
+    }
+
+    let counter = 1 // tracker to add values into matrix
+    let startColumn = 0
+    let endColumn = n - 1
+    let startRow = 0
+    let endRow = n - 1
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+        // build the Topmost Row
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[ startRow ][ i ] = counter
+            counter++
+        }
+
+        // build the column on the half-bottom right
+        startRow++
+        for (let i = startRow; i <= endRow; i++) {
+            results[ i ][ endColumn ] = counter
+            counter++
+        }
+
+        // Bottom Row (beyond the last column)
+        endColumn--
+        for (let i = endColumn; i >= startColumn; i--) {
+            results[ endRow ][ i ] = counter
+            counter++
+        }
+
+        // move to one row up above
+        endRow--
+
+        // build the start column
+        for (let i = endRow; i >= startRow; i--) {
+            results[ i ][ startColumn ] = counter
+            counter++
+        }
+
+        // move inwards from the start column
+        startColumn++
+    }
+
+    return results
+}
+
+module.exports = matrix
