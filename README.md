4e3e4955cfd6fd6ead719702b9a74e8dde21fe65
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-07-18 10:17:53 +0545

    Finished 'fib' iterative and recursive solutions

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Data structures and Algorithms (using JavaScript)
 
-> > View folders within exercises folder for various DSA codes
+> View folders within exercises folder for various DSA codes
+
+## Runtime complexities
+
+1. ReverseString: linear => O(n)
+2. printing Steps (the iterative, for loop solution): Quadratic => O(n^2)
+3. printing Pyramid (the iterative, for loop solution): Quadratic => O(n^2)
+4. Nth Fibonacci number (iterative solution): O(n)
--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,40 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+    if (n == 0 || n == 1) {
+        return n
+    }
 
-module.exports = fib;
+    // MY SOLUTION 1: ITERATIVE
+    /*let fibCounter = 2 // this example is 0-indexed
+    let prev = 0, cur = 1
+    while (fibCounter <= n) {
+        const temp = prev + cur
+        prev = cur
+        cur = temp
+
+        fibCounter++
+    }
+
+    return cur*/
+
+    // * REF SOLUTION 1 ITERATIVE
+    /*const result = [ 0, 1 ]
+
+    for (let i = 2; i <= n; i++) {
+        result.push(result[ i - 1 ] + result[ i - 2 ])
+    }
+
+    return result[ result.length - 1 ] // result[n]
+    */
+
+    // REF SOLUTION 2 RECURSIVE
+    if (n == 0 || n == 1) { // n < 2
+        return n
+    }
+
+    return fib(n - 1) + fib(n - 2)
+}
+
+module.exports = fib
