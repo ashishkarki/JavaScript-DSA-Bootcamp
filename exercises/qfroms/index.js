ccbd172620cf48dde23445fcde8457f9ee7f7fe0
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-07-19 11:47:23 +0545

    Finished 'qfroms' queue from stack implementation

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,74 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require('./stack')
 
-class Queue {}
+class Queue {
+    constructor () {
+        this.addStack = new Stack()
+        this.removeStack = new Stack()
+    }
 
-module.exports = Queue;
+    add(element) {
+        this.addStack.push(element)
+    }
+
+    remove() {
+        while (this.addStack.peek()) {
+            this.removeStack.push(this.addStack.pop())
+        }
+
+        const removedElem = this.removeStack.pop()
+
+        // restore addStack
+        while (this.removeStack.peek()) {
+            this.addStack.push(this.removeStack.pop())
+        }
+
+        return removedElem
+    }
+
+    peek() {
+        while (this.addStack.peek()) {
+            this.removeStack.push(this.addStack.pop())
+        }
+
+        const peekedElem = this.removeStack.peek()
+
+        // restore addStack
+        while (this.removeStack.peek()) {
+            this.addStack.push(this.removeStack.pop())
+        }
+
+        return peekedElem
+    }
+
+}
+
+module.exports = Queue
+
+// MY IMPLEMENTATION COMMENTED 
+/*
+add(element) {
+        this.addStack.push(element)
+    }
+
+    remove() {
+        if (this.removeStack.peek()) {
+            return this.removeStack.pop()
+        } else if (this.addStack.peek()) {
+            while (this.addStack.peek()) {
+                this.removeStack.push(this.addStack.pop())
+            }
+
+            return this.removeStack.pop()
+        } else {
+
+            return undefined
+        }
+    }
+
+    peek() {
+        return this.removeStack.peek()
+    }
+*/
