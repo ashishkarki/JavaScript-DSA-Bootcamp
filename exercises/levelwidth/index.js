f9059d0a44f096383d77207c2d15bb74551b2f25
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-07-20 19:15:15 +0545

    Finished 'levelWidth'

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,32 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const END_OF_LEVEL_TOKEN = 'end-of-level'
+    const widths = [ 0 ]
+    const array = [ root, END_OF_LEVEL_TOKEN ]
 
-module.exports = levelWidth;
+    while (array.length > 1) {// there must be something more than the end of level token in the array
+        const topNode = array.shift()
+
+        // new level is starting
+        if (topNode === END_OF_LEVEL_TOKEN) {
+            // move the token to end of array
+            array.push(END_OF_LEVEL_TOKEN)
+
+            // create a new widths array entry
+            widths.push(0)
+        } else {
+
+            // if it is a regular node, push its children into array
+            array.push(...topNode.children)
+
+            // increase the last entry in widths to indicate that we found a new node at this level
+            widths[ widths.length - 1 ] += 1
+        }
+    }
+
+    return widths
+}
+
+module.exports = levelWidth
